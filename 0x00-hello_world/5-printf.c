1d2c2beaf7bffe62e3f51aaf1cb9356713619242
Lemi <leminegeso@gmail.com>
2023-10-07 12:43:32 -0700

    printing some text

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+/**
+*main Entry point
+*
+*Description: prints some characters "with proper grammar, but the outcome is a piece of art,"
+*
+*Return: always return 0 (success)
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
