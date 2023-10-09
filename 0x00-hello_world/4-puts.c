555f89cfc535a59843aa46e6158b6ebc619910a5
Lemi <leminegeso@gmail.com>
2023-10-09 02:19:42 -0700

    Sizeof calculator

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,11 @@
 #include <stdio.h>
+/**
+ *main - Entry point
+ *
+ *puts: prints some character and ends in new line
+ *
+ *Return: returns 0 (successs)
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
-#include<stdio.h>
+#include <stdio.h>
 /**
-*main Entry point
-*
-*Description: prints some characters "with proper grammar, but the outcome is a piece of art,"
-*
-*Return: always return 0 (success)
-*/
+ *main - Entry point
+ *
+ *Description: prints some character
+ *
+ *Return: always return 0 (success)
+ */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
