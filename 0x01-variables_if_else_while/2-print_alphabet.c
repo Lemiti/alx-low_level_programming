e490747165cade9b75b71a8f07e456a28abcff6b
Lemi <leminegeso@gmail.com>
2023-10-09 17:27:15 -0700

    new commit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ *main - entry point
+ *
+ *Description: sorfjjo
+ *
+ *Return: returns 0
+ */
+int main(void)
+{
+	int n = 97;
+
+	while (n <= 122)
+	{
+		putchar(n);
+		n++;
+	}
+	putchar("\n");
+	return (0);
+}
