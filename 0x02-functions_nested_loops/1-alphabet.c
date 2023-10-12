8255676fee19cf94332112b89a76b4f1b6d8da58
Lemi <leminegeso@gmail.com>
2023-10-11 19:35:03 -0700

    prints lowercase alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ *print_alphabet - prints all lowercase alphabet
+ */
+void print_alphabet(void)
+{
+	char l;
+
+	for (l = 'a'; l <= 'z'; l++)
+		_putchar(l);
+
+	_putchar('\n');
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,4 +1,7 @@
-#ifndef _PUTCHAR_C
-#define _PUTCHAR_C
+#ifndef MAIN_H
+#define MAIN_H
 int _putchar(char c);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
 #endif
