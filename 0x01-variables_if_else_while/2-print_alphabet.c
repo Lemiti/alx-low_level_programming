a1daf031638e0df65f035d381bd227fee8ecbe97
Lemi Negeso <leminegeso@gmail.com>
2024-06-10 14:39:10 -0700

    fix my repo

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+	int x=97;
+	while (x<=122)
+	{
+		putchar(x);
+		x ++;
+	}
+	putchar(10);
+	return 0;
+}
+
+
---
+++ README.md
@@ -0,0 +1,1 @@
+This is my jorny in learning low-level programming at alx.
