1a6b837e1780b1b053c896b16f1477c66d292196
Lemi Negeso <leminegeso@gmail.com>
2024-06-17 16:33:02 -0700

    Edit 2-print_alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,15 +1,21 @@
 #include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0 on success;
+ */
 
-int main()
+int main(void)
 {
-	int x=97;
-	while (x<=122)
+	int x = 97;
+
+	while (x <= 122)
 	{
 		putchar(x);
-		x ++;
+		x += 1;
 	}
 	putchar(10);
-	return 0;
+	return (0);
 }
 
 
