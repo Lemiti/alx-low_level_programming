bd0b059df57f3724ad0b19f22de173ec2dcb2fed
Lemi Negeso <leminegeso@gmail.com>
2024-06-22 09:06:20 -0700

    try

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0 On success
+ */
+int main(void)
+{
+	int 
---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,14 @@
+#include <main.h>
+/**
+ * _isalpha - Checks if the
+ *@c: this is intiger value
+ * Return: return 0 or 1
+ */
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	{
+		return (1);
+	}
+	return (0);
+}
