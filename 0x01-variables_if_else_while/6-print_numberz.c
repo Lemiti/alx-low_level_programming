bb46a8e9353a4c02e76477224da128ee33ec92b0
Lemi Negeso <leminegeso@gmail.com>
2024-06-22 02:20:43 -0700

    done task 6 and 7

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point 
+ *
+ * Return: 0 on success
+ */
+
+int main(void)
+{
+	int x;
+
+	for (x = 48; x < 58; x++)
+	{
+		putchar(x);
+
+	}
+	putchar('\n');
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0 on success
+ */
+
+int main(void)
+{
+	int x;
+
+	for (x = 122; x >= 97; x--)
+	{
+		putchar(x);
+	}
+	putchar('\n');
+	return (0);
+}
