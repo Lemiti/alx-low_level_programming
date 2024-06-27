4729be9b03b6c6d320b834d34407cd9502bf740f
Lemi Negeso <leminegeso@gmail.com>
2024-06-27 11:04:59 -0700

    try 101 task

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -6,4 +6,32 @@
  */
 int main(void)
 {
-	int 
+	int x;
+
+	int y;
+
+	int z;
+
+	for (x = 48; x <= 55; x++)
+	{
+		for (y = 49; y <= 56; y++)
+		{
+			for (z = 50; z <= 57; z++)
+			{
+				if (x < y && y < z)
+				{
+					putchar(x);
+					putchar(y);
+					putchar(z);
+					if (x != 55)
+					{
+						putchar(44);
+						putchar(32);
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
