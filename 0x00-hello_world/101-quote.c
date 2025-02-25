b29136fd50cc621ca49788228672e302e2755f64
Lemi <leminegeso@gmail.com>
2025-02-26 00:28:55 +0300

    add 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,13 @@
-#include <stdio.h>
+#include <unistd.h>
 /**
  * main - Entry point
  *
- * Return:1 On success
+ * Return:1 On failer
  */
 
 int main(void)
 {
-	char st[30] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
-	for (x a
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+
+	return (1);
+}
