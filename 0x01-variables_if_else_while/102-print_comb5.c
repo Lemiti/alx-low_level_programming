1e3d3ef855b0c0a5bf1ecb7a58c96808963f3f3f
Lemi <leminegeso@gmail.com>
2025-02-27 01:06:32 +0300

    again 102-print_coms.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,36 +1,39 @@
 #include <stdio.h>
-
 /**
  * main - Entry point
  *
  * Return:0 on success
  */
-
 int main(void)
 {
 	int i, j, d, c;
-	
 
 	for (i = 48; i <= 57; i++)
 	{
-		for (j = 48; j < 57; j++)
+		for (j = 48; j <= 57; j++)
 		{
+			if (i == 57 && j == 57)
+			{
+				break;
+			}
 			for (d = i; d <= 57; d++)
 			{
 				int yazh = 0;
-				do {	
+
+				do {
 					if (d == i && yazh == 0)
 					{
 						c = j + 1;
-					}else if (yazh == 0) {
+					}
+					else if (yazh == 0)
+					{
 						c = 48;
-					} 
+					}
 					putchar(i);
 					putchar(j);
 					putchar(32);
 					putchar(d);
 					putchar(c);
-					
 					if (!(i == 57 && j == 56))
 					{
 						putchar(44);
@@ -38,7 +41,7 @@ int main(void)
 					}
 					++c;
 					++yazh;
-				} while(c <= 57);
+				} while (c <= 57);
 			}
 		}
 	}
