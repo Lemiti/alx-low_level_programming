432d7bf370d7387b2bfbc9855b18c6a7d3fbb138
Lemi <leminegeso@gmail.com>
2025-02-28 23:03:52 +0300

    Add 1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return:0 on success
+ */
+
+int main(void)
+{
+	int i = 97;
+
+	for (; i < 97; i++)
+	{
+		_putchar(i);
+	}
+
+	return (0);
+}
