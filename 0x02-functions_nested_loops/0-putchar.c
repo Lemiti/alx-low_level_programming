0cda0656ef0109ae06f32920ef24bea1d1598a71
Lemi <leminegeso@gmail.com>
2025-03-01 07:10:20 +0300

    fix 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
  * main - Entry point
@@ -8,5 +8,17 @@
 
 int main(void)
 {
+	char my_c[8] = {'_', 'p', 'u', 't', 'c', 'h', 'a', 'r'};
+
+	int i = 0;
+
+	for (; i < 8; i++)
+	{
+		_putchar(my_c[i]);
+	}
+
+	_putchar('\n');
+
+	return (0);
 
 }
