dbcc59326590ff3192c90bd071e01de75e0fc74e
Lemi <leminegeso@gmail.com>
2025-03-01 21:52:21 +0300

    add 3-main.c, 5-main.c and edit 4-main.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _islower - checks whether character is lower or not
+ * Return:1 if the character is in lowercase
+ * Return:0 if the character is in not lowercase
+ */
+
+int _islower(int c)
+{
+	
+}
