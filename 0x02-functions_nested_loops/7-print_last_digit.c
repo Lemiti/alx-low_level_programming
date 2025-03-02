809ac44fd47ac799c68ae34ec800049f643021da
Lemi <leminegeso@gmail.com>
2025-03-02 22:24:56 +0300

    add 8-main.c and 9-main.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_last_digit - writes last digit
+ * @n: inputed number
+ * Return: last number digit
+ */
+
+int print_last_digit(int n)
+{
+	int a;
+
+	a = n%10;
+	return (a);
+}
