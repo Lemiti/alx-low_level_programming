89058cd00bc12c5e950e6c4e02143d06581d0331
Lemi <leminegeso@gmail.com>
2025-03-03 13:20:26 +0300

    compile 7-last_digit again

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * jack_bauer - Prints every minute of the day
+ * Return:void or nothing
+ */
+
+void jack_bauer(void)
+{
+	int h, h2, min, min2;
+	
+	for (h = 0; h <= 2; h++)
+	{
+		do {
+			if (h == 2 && h2 >3)
+			{
+				break;
+			}
+			for (min = 0; min < 6; min++)
+                        {
+                                for (min2 = 0; min2 <= 9; min2++)
+                                {
+                                        _putchar('0' + h);
+                                        _putchar('0' + h2);
+                                        _putchar(58);
+                                        _putchar('0' + min);
+                                        _putchar('0' + min2);
+                                        _putchar(10);
+                                }
+                        }
+			h2 += 1;
+		}while(h2<=9);
+	}
+}
