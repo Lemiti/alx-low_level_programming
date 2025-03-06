74515b8b6dec8d9b0938d5178b89be1279ef9d90
Lemi <leminegeso@gmail.com>
2025-03-06 23:46:23 +0300

    begin 0x04

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _isupper - checks wether the inputed value is capital or not
+ * @c: input variable 
+ * Return:1 if c is upper case 0 otherwise
+ */
+
+int _isupper(int c)
+{
+	if (
+}
