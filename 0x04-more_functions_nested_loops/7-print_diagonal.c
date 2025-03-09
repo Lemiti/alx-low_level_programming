8ea8bbea07b5522a57f5539998d0885f8b48a9e0
Lemi <leminegeso@gmail.com>
2025-03-09 23:10:44 +0300

    fix 7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -6,4 +6,20 @@
  */
 void print_diagonal(int n)
 {
+	int y, i;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	for (y = 0; y < n; y++)
+	{
+		for (i = 0; i < y; i++)
+		{
+			
+			_putchar(' ');
+		}
+		_putchar('\\');
+		_putchar('\n');
+	}
 }
