a33423919c780803735cd6c137bcd1928ef8b485
Lemi <leminegeso@gmail.com>
2025-03-10 23:23:44 +0300

    add 10-print_triangle.c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -2,4 +2,23 @@
 
 /**
  * print_triangle - discplay triangle
- * @sisze: determines size of triangle
+ * @size: determines size of triangle
+ */
+void print_triangle(int size)
+{
+	int i, j, a;
+
+	for (i = 1; i <= size; i++)
+	{
+		for (j = size - i; j >= 0; j--)
+		{
+			_putchar(' ');
+		}
+		for (a = 0; a < i; a++)
+		{
+			_putchar('#');
+		}
+
+		_putchar('\n');
+	}
+}
