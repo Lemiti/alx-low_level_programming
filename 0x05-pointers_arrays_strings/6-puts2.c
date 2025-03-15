eb56ccb06fbdc92620a309650d2b005c1cfef0bc
Lemi <leminegeso@gmail.com>
2025-03-15 22:06:41 +0300

    fix 6-puyt2 and 7-puts_half

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -7,15 +7,12 @@
 
 void puts2(char *str)
 {
-	int i, n;
+	int i;
 
 	while (str[i] != '\0')
 	{
-		i++;
-	}
-	for (n = 0; n < i; n += 2)
-	{
-		_putchar(str[n]);
+		_putchar(str[i]);
+		i += 2;
 	}
 	_putchar('\n');
 }
