4129a6a5375e3a7d45f08f79184dce94b24de504
Lemi <leminegeso@gmail.com>
2025-03-16 23:38:43 +0300

    Revert "fix 6.. "
    This reverts commit 812ccf3928611ff0b8bd7ac5b10c7e8bd56b2122.

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,5 +1,5 @@
 #include "main.h"
-#include <stdio.h>
+
 /**
  * puts2 - prints every characters wich are found in odd
  * @str: variable to hold string
@@ -8,9 +8,6 @@
 void puts2(char *str)
 {
 	int i;
-
-	 if (str == NULL)
-        return;
 
 	while (str[i] != '\0')
 	{
