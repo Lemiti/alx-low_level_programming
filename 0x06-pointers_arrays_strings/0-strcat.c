13e1cb7c143ed21f027a65c77ddf0673241deebf
Lemi <leminegeso@gmail.com>
2025-03-19 20:25:26 +0300

    fix 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,1 +1,30 @@
-#include <studio>
+#include "main.h"
+
+/**
+ * _strcat - concatenates two strings
+ * @dest: the first string
+ * @src: the second string
+ * Return: characters
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	char *check;
+
+	check = dest;
+
+	while (*dest != '\0')
+	{
+		dest++;
+	}
+	while (*src != '\0')
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+	*dest = '\0';
+
+
+	return (check);
+}
