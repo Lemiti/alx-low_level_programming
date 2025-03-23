6f0fd315930a78111252cf5dfb5974ab14f1050d
Lemi <leminegeso@gmail.com>
2025-03-24 00:32:00 +0300

    fix 2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,1 +1,35 @@
-#include 
+#include "main.h"
+
+/**
+ * _strncpy - copies a string
+ * @dest: the addres to be copied on
+ * @src: the string to be copied
+ * @n: the maximum byte of string src
+ * Return: copied string pointer
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	char *ptr = dest;
+
+	while (*src != '\0' && n >= 0)
+	{
+		*dest = *src;
+		dest++;
+		src++;
+		n--;
+	}
+
+	while (n > 0)
+	{
+		*dest = '\0';
+		dest++;
+		n--;
+	}
+
+	return (ptr);
+
+	*dest = '\0';
+
+	return (dest);
+}
