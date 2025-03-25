ba2578f4c9c5289d1038da3404145fc84a30576e
Lemi <leminegeso@gmail.com>
2025-03-25 23:48:19 +0300

    begin 3-strcmp.c

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * _strcmp - compares two strings
+ * @s1: the first string
+ * @s2: the second string
+ * Return: integer
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+}
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,5 +1,7 @@
 #ifndef MAIN_HEADER
 #define MAIN_HEADER
+char *_strcat(char *dest, char *src, int n);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
 #endif
